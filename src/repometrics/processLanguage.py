457d8bd4edfcb39aa68a9a636a867324172a3add
Vincent Setiawan <setiawan.vincent@rocketmail.com>
2021-12-21 01:00:19 +0800

    fixed formatting

--- src/repometrics/processLanguage.py
+++ src/repometrics/processLanguage.py
@@ -1,14 +1,26 @@
 import json
 
-def processLanguage(filename="languages.json"):
-    f = open(filename)
-    data = json.load(f)
 
+def processLanguage(filename: str = "languages.json", out_file: str = "extensionLanguage.json") -> None:
+    """
+    This function processes JSON file containing mapping from languages to its related extensions into a JSON file mapping extensions to the language that it belongs to.
+
+    """
+    # Load the file into a dictionary
+    with open(filename, 'r') as f:
+        data = json.load(f)
+
+    # Map the extension to the language
     extToLanguageDict = {}
-
     for key in data.keys():
         if "extensions" in data[key]:
             for extension in data[key]["extensions"]:
                 extToLanguageDict[extension] = key
 
-    return extToLanguageDict
+    # Save the file
+    with open(out_file, 'w') as f:
+        json.dump(extToLanguageDict, f, indent=4)
+
+
+if __name__ == "__main__":
+    processLanguage()
